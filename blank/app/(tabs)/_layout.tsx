632c2c678ba5805dda6d0584836604c2e465e248
Adir <slutzkis@gmail.com>
2024-08-22 13:50:02 +0300

    updates

--- blank/app/(tabs)/_layout.tsx
+++ blank/app/(tabs)/_layout.tsx
@@ -1,6 +1,6 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-import First from './index';
+import First from './Tab_01';
 
 import { TabBarIcon } from '@/components/navigation/TabBarIcon';
 import { Colors } from '@/constants/Colors';
